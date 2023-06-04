9867859cdcef08caafedebdc19a6e3b1f02e62c7
MAK 18 <malikhawaja18@gmail.com>
2023-06-04 16:22:07 +0500

    Added Node Modules

---
+++ degree.sol
@@ -0,0 +1,38 @@
+pragma solidity >0.8.0;
+pragma experimental ABIEncoderV2;
+
+contract degree
+{
+    string public reg_no;
+    string public name;
+    string public father_Name;
+    string public cgpa;
+    struct subject
+    {
+        string course_code;
+        string course_name;
+        uint8 credit_hours;
+        string grade;
+    }
+    subject[] courses;
+    constructor (string memory reg_no1, string memory name1, string memory father_name1, string memory cgpa1) public   
+    {
+         reg_no = reg_no1;
+         name=name1;
+         father_Name=father_name1;
+         cgpa=cgpa1;
+    }
+
+    function addsubject(string memory course_code1, string memory course_name1, uint8 credit_hours1, string memory grade1) public
+    {
+        courses.push(subject(course_code1, course_name1, credit_hours1, grade1));
+    }
+
+    function getsubjects() public view returns(subject[] memory)
+    {
+        return courses;
+    }
+
+}
+
+
--- web.js
+++ web.js
@@ -11,14 +11,14 @@ web3 = new Web3(new Web3.providers.HttpProvider("https://cf11-2400-adc5-131-8d00
 
 // reading the file contents of the smart  contract
 
-fileContent = fs.readFileSync("demo.sol").toString();
+fileContent = fs.readFileSync("degree.sol").toString();
 console.log(fileContent);
 
 // create an input structure for my solidity compiler
 var input = {
   language: "Solidity",
   sources: {
-    "demo.sol": {
+    "degree.sol": {
       content: fileContent,
     },
   },
@@ -35,7 +35,7 @@ var input = {
 var output = JSON.parse(solc.compile(JSON.stringify(input)));
 console.log("Output: ", output);
 
-ABI = output.contracts["demo.sol"]["demo"].abi;
-bytecode = output.contracts["demo.sol"]["demo"].evm.bytecode.object;
+ABI = output.contracts["degree.sol"]["degree"].abi;
+bytecode = output.contracts["degree.sol"]["degree"].evm.bytecode.object;
 console.log("Bytecode: ", bytecode);
 console.log("ABI: ", ABI);
